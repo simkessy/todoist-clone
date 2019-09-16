080b5e8b27e44cb072215a349a1fc0eb635626fb
Kessy Similien <simkessy@gmail.com>
2019-09-16 13:31:52 -0700

    HTML updates

--- src/component/layouts/Header.js
+++ src/component/layouts/Header.js
@@ -6,12 +6,17 @@ export const Header = () => {
     <header className="header">
       <nav>
         <div className="logo">
-          <img src="/images/logo.png" alt="Todoist" />
+          <img
+            src="https://d3ptyyxy2at9ui.cloudfront.net/favicon-a85885.ico"
+            alt="Todoist"
+          />
         </div>
         <div className="settings">
           <ul>
-            <li>+</li>
-            <li>
+            <li data-testid="quick-add-task-action" className="settings__add">
+              +
+            </li>
+            <li data-testid="dark-mode-action" className="settings__darkmode">
               <FaPizzaSlice />
             </li>
           </ul>
